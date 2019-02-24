e416d0cf497cce3224e080bdb11b669bb2235c2d
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2019-02-23 20:38:06 -0800

    run sim now reads from files for strategies

--- run_sim.py
+++ run_sim.py
@@ -1,13 +1,37 @@
 import sim
 import json
 
+# number of nodes that are submitted
+n = 5
+
 # read in the graph
 with open('testgraph1.json') as f:
     graph = json.load(f)
 
-# generate nodes
-strategies = {'strat1' : ['83', '463', '319', '149', '254'], \
-            'strat2': ['447', '164', '301', '369', '222']}
+# read in strategy 1
+f1 = open("submission.txt", "r")
+sub1 = []
+i = 0
+for line in f1:
+    if i >= n:
+        break;
+
+    sub1.append(line.strip())
+
+
+# read in strategy 2
+f2 = open("submission.txt", "r")
+sub2 = []
+j = 0
+for line in f2:
+    if j >= n:
+        break;
+
+    sub2.append(line.strip())
+
+# submissions for the strategies
+strategies = {'strat1' : sub1, \
+            'strat2': sub2}
 
 # run sim
 result = sim.run(graph, strategies)
